148acf2c1ca4bb6e7be86a79f5acdde914e46905
jonas-brink <jobrink@uos.de>
2021-04-17 23:18:04 +0200

    changed design

--- grafikinterface_new.py
+++ grafikinterface_new.py
@@ -5,7 +5,7 @@ import time
 
 class mainGui:
 	def __init__(self, master, mode):
-		self.Max = Person("Max", "Mustermann", "00.00.2000", "123456789", "Alleee", "120", "32584", "Löhne")
+		self.noOne = Person(" ", " ", " ", " ", " ", " ", " ", " ")
 		self.master = master
 		self.counter = 0
 
@@ -14,7 +14,7 @@ class mainGui:
 		elif mode==2:
 			master.geometry("1280x1024")
 
-		master.config(bg="gray")
+		master.config(bg="#202020")
 		master.title("Main Interface")
 		master.resizable(True, True)
 		master.columnconfigure(0, weight=1)
@@ -23,13 +23,13 @@ class mainGui:
 		master.rowconfigure(1, weight=1)
 
 		#building frame grid
-		self.frameTop = Frame(master, padx=30, pady=10, bg='orange')
+		self.frameTop = Frame(master, padx=30, pady=10, bg='#202020')
 		self.frameTop.grid(row=0, column=0, sticky=E+W+N+S)
-		self.frameTopRight = Frame(master, padx=30, pady=10, bg='red')
+		self.frameTopRight = Frame(master, padx=30, pady=10, bg='#202020')
 		self.frameTopRight.grid(row=1, column=0, sticky=E+W+N+S)
-		self.frameBot = Frame(master, padx=30, pady=10, bg='gray')
+		self.frameBot = Frame(master, padx=30, pady=10, bg='#202020')
 		self.frameBot.grid(row=0, column=1, rowspan=2, sticky=E+W+N+S)
-		self.frameBotRight = Frame(master, padx=30, pady=10, bg='gray')
+		self.frameBotRight = Frame(master, padx=30, pady=10, bg='#202020')
 		#self.frameBotRight.grid(row=1, column=1, sticky=E+W+N+S)
 
 		self.frameTop.rowconfigure(0, weight=1)
@@ -85,7 +85,7 @@ class mainGui:
 		#	self.scrollbar2.pack(side=RIGHT, fill="y")
 
 		# create output
-		self.output = Listbox(self.frameTerminal, yscrollcommand=self.scrollbar.set, width=50, height=30, background='black', fg='white')
+		self.output = Listbox(self.frameTerminal, yscrollcommand=self.scrollbar.set, width=50, height=30, background='black', fg='white', font=('Helvetica', 14))
 		self.output.grid(row=0, column=0, sticky='NSEW')
 		self.scrollbar.config(command=self.output.yview)
 
@@ -107,18 +107,15 @@ class mainGui:
 
 			self.bausetllaLabel22 = Label(self.frameTopRight, text="Aktuell Erkannt:", bg="black", fg="white",font=("Courier", 16), width="45").grid(row=0, column=0, columnspan = 2, padx='5', pady='0', sticky=E+W+N+S)
 			self.bausetllaLabel221 = Label(self.frameTopRight, text="Name:", bg="black", fg="white", font=("Courier", 12), width="15").grid(row=1, column=0, padx='5', pady='0', sticky=E+W+N+S)
-			self.bausetllaLabel222 = Label(self.frameTopRight, text="Max Mustermann", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=1, column=1, padx='5', pady='0',sticky=E+W+N+S)
+			self.bausetllaLabel222 = Label(self.frameTopRight, text=" ", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=1, column=1, padx='5', pady='0',sticky=E+W+N+S)
 			self.bausetllaLabel223 = Label(self.frameTopRight, text="Geburtsdatum:", bg="black", fg="white", font=("Courier", 12), width="15").grid(row=2, column=0, padx='5', pady='0', sticky=E+W+N+S)
-			self.bausetllaLabel224 = Label(self.frameTopRight, text="01.01.2000", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=2, column=1, padx='5', pady='0', sticky=E+W+N+S)
+			self.bausetllaLabel224 = Label(self.frameTopRight, text=" ", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=2, column=1, padx='5', pady='0', sticky=E+W+N+S)
 			self.bausetllaLabel225 = Label(self.frameTopRight, text="Telefonnummer:", bg="black", fg="white", font=("Courier", 12), width="15").grid(row=3, column=0, padx='5', pady='0', sticky=E+W+N+S)
-			self.bausetllaLabel226 = Label(self.frameTopRight, text="0123456789", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=3, column=1, padx='5', pady='0', sticky=E+W+N+S)
+			self.bausetllaLabel226 = Label(self.frameTopRight, text=" ", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=3, column=1, padx='5', pady='0', sticky=E+W+N+S)
 			self.bausetllaLabel227 = Label(self.frameTopRight, text="Straße & Hausnummer:", bg="black", fg="white", font=("Courier", 12), width="15").grid(row=4, column=0, padx='5', pady='0', sticky=E+W+N+S)
-			self.bausetllaLabel228 = Label(self.frameTopRight, text="Alleee 120", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=4, column=1, padx='5', pady='0', sticky=E+W+N+S)
+			self.bausetllaLabel228 = Label(self.frameTopRight, text=" ", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=4, column=1, padx='5', pady='0', sticky=E+W+N+S)
 			self.bausetllaLabel229 = Label(self.frameTopRight, text="PLZ & Ort:", bg="black", fg="white",font=("Courier", 12), width="15").grid(row=5, column=0, padx='5', pady='0',sticky=E+W+N+S)
-			self.bausetllaLabel2210 = Label(self.frameTopRight, text="32584 Löhne", bg="black", fg="white", font=("Courier", 12),width="30").grid(row=5, column=1, padx='5', pady='0', sticky=E+W+N+S)
-			self.bausetllaLabel2211 = Label(self.frameTopRight, text="Uhrzeit der Erfassung:", bg="black", fg="white",font=("Courier", 12), width="15").grid(row=5, column=0, padx='5', pady='0',sticky=E+W+N+S)
-			self.bausetllaLabel2212 = Label(self.frameTopRight, text="00:00 Uhr", bg="black", fg="white", font=("Courier", 12),width="30").grid(row=5, column=1, padx='5', pady='0', sticky=E+W+N+S)
-
+			self.bausetllaLabel2210 = Label(self.frameTopRight, text=" ", bg="black", fg="white", font=("Courier", 12),width="30").grid(row=5, column=1, padx='5', pady='0', sticky=E+W+N+S)
 			self.bausetllaLabel23 = Label(self.frameBot, text="Anwesende Personen", bg="black", fg="green").grid(row=0, column=0, padx='5', pady='5', sticky='ew')
 
 			#self.bausetllaLabel24 = Label(self.frameBotRight, text="Abwesende Personen", bg="black", fg="red").grid(row=0, column=0, padx='5', pady='5', sticky='ew')
@@ -136,6 +133,7 @@ class mainGui:
 		for person in persons:
 			self.output.insert(END, str(person.__repr__()))
 			self.output.see("end")
+		self.setAnzahlPersonen(len(persons))
 
 	def anzahlPersonenErhöhen(self):
 		self.counter = self.counter +1
@@ -145,10 +143,13 @@ class mainGui:
 		self.counter = self.counter -1
 		self.bausetllaLabel213 = Label(self.frameTop, text=str(self.counter), bg="blue", fg="white", font=("Courier", 16)).grid(row=2, column=1, padx='5', pady='5', sticky='ew')
 
+	def setAnzahlPersonen(self, anzahl):
+		self.counter = anzahl
+		self.bausetllaLabel213 = Label(self.frameTop, text=str(self.counter), bg="blue", fg="white", font=("Courier", 16)).grid(row=2, column=1, padx='5', pady='5', sticky='ew')
+
 	def personGescannt(self, person):
 		self.frameTopRight.config(bg='green')
 		self.personAnzeigen(person)
-		self.anzahlPersonenErhöhen()
 		#self.bausetllaLabel211 = Label(self.frameTop, text="Warten : 5", bg="red", fg="white", font=("Courier", 22)).grid(row=1, column=0, columnspan=2, padx='5', pady='5', sticky='ew')
 		#self.master.update()
 		#time.sleep(1)
@@ -166,8 +167,8 @@ class mainGui:
 		time.sleep(1)
 		self.bausetllaLabel211 = Label(self.frameTop, text="Bereit", bg="green", fg="white", font=("Courier", 22)).grid(row=1, column=0, columnspan=2, padx='5', pady='5', sticky='ew')
 		self.master.update()
-		self.personAnzeigen(self.Max)
-		self.frameTopRight.config(bg='red')
+		self.personAnzeigen(self.noOne)
+		self.frameTopRight.config(bg='#202020')
 
 	def personAnzeigen(self, person):
 		self.bausetllaLabel222 = Label(self.frameTopRight, text=person.vorname.replace('\\', '')+" "+person.nachname.replace('\\', ''), bg="black", fg="white",font=("Courier", 12), width="30").grid(row=1, column=1, padx='5', pady='0',sticky='ew')
@@ -175,8 +176,6 @@ class mainGui:
 		self.bausetllaLabel226 = Label(self.frameTopRight, text=person.telefonnr.replace('\\', ''), bg="black", fg="white", font=("Courier", 12), width="30").grid(row=3, column=1, padx='5', pady='0', sticky='ew')
 		self.bausetllaLabel228 = Label(self.frameTopRight, text=person.strasse.replace('\\', '')+" "+person.hausnr.replace('\\', ''), bg="black", fg="white", font=("Courier", 12), width="30").grid(row=4, column=1, padx='5', pady='0', sticky='ew')
 		self.bausetllaLabel2210 = Label(self.frameTopRight, text=person.plz.replace('\\', '')+" "+person.ort.replace('\\', ''), bg="black", fg="white", font=("Courier", 12), width="30").grid(row=5, column=1, padx='5', pady='0', sticky='ew')
-		self.bausetllaLabel2212 = Label(self.frameTopRight, text="00:00 Uhr", bg="black", fg="white", font=("Courier", 12), width="30").grid(row=5, column=1, padx='5', pady='0', sticky='ew')
-
 
 	#def write2(self, txt):
 	#	if txt == "BLANK":
--- startGrafik.py
+++ startGrafik.py
@@ -5,7 +5,7 @@ class startGui:
 	def __init__(self, master):
 		self.master = master
 
-		master.geometry("720x320+600+300")
+		master.geometry("1280x1024")
 		master.config(bg="white")
 		master.title("Info")
 		master.resizable(False, False)
@@ -13,14 +13,14 @@ class startGui:
 		self.frameTop1 = Frame(master, padx=50, pady=50)
 		self.frameTop1.grid(row=0, column=0)
 
-		self.infoLabel1 = Label(self.frameTop1, text="Info", bg="black", fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
+		self.infoLabel1 = Label(self.frameTop1, text="Info", bg='#202020', fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
 
 	def auswahlTreffen(self):
-		self.infoLabel1 = Label(self.frameTop1, text="Wähle Modus (A: Scannen, D: Drucken): ", bg="black", fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
+		self.infoLabel1 = Label(self.frameTop1, text="Wähle Modus (A: Scannen, D: Drucken): ", bg='#202020', fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
 
 	def datumEingeben(self):
-		self.infoLabel1 = Label(self.frameTop1, text="Bitte das Datum eingeben (TT*MM*JJJJ): ", bg="black", fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
+		self.infoLabel1 = Label(self.frameTop1, text="Bitte das Datum eingeben (TT*MM*JJJJ): ", bg='#202020', fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
 
 	def uhrzeitEingeben(self):
-		self.infoLabel1 = Label(self.frameTop1, text="Bitte die Uhrzeit eingeben (HH*MM): ", bg="black", fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
+		self.infoLabel1 = Label(self.frameTop1, text="Bitte die Uhrzeit eingeben (HH*MM): ", bg='#202020', fg="white", font=("Courier", 22)).grid(row=0, column=0, padx='5', pady='5', sticky='ew')
 
