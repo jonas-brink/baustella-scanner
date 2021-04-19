1138478e6cba5f6709934e80ce091691811a149f
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2021-04-19 16:54:59 +0200

    Threading

--- Test.py
+++ Test.py
@@ -1,13 +1,17 @@
 import grafikinterface
+import threading
 from tkinter import *
 
-if __name__ == "__main__":
+def thread_function():
     main = Tk()
     gui = grafikinterface.mainGui(main)
     gui.write("test")
     gui.write2("test2")
     main.mainloop()
 
+if __name__ == "__main__":
+    x = threading.Thread(target=thread_function())
+    x.start()
 
 
 
