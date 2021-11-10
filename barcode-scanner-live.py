ea890718c0013cbe20809db6e94d4e0eb4b9266e
Jonas Brink <jobrink@uos.de>
2021-11-10 19:56:47 +0100

    refactoring

--- barcode-scanner-live.py
+++ barcode-scanner-live.py
@@ -1,6 +1,5 @@
 #export LC_ALL=de_DE.UTF-8
 # import packages
-import cv2
 from pyzbar import pyzbar
 import imutils
 from imutils.video import VideoStream
@@ -8,7 +7,7 @@ import time
 import pickle
 import threading
 from tkinter import *
-import grafikinterface_new
+import scanGraphic
 import re
 from person import Person
 # get access to LEDs connected to RPi
@@ -18,9 +17,8 @@ import keypad as Keypad
 import RPi.GPIO as GPIO
 from tabulate import tabulate
 import pdfkit
-import startGrafik
+import indexGraphic
 from urllib.parse import unquote
-import os
 
 
 ROWS = 4
@@ -29,6 +27,7 @@ keys = ['1', '2', '3', 'A',
         '4', '5', '6', 'B',
         '7', '8', '9', 'C',
         '*', '0', '#', 'D']
+# save pins of keypad occupancy
 rowsPins = [7, 8, 11, 25]
 colsPins = [9, 10, 15, 18]
 
@@ -80,89 +79,86 @@ def scan(gui, dateFile, pin):
     time.sleep(2.0)
     print('Start...')
 
-    # loop over frames
+    # loop over all frames
     while True:
         frame = vs.read()
-        # resize image for better performance
-        # frame = imutils.resize(frame, width=1000)
+        # read QR codes in frame
         barcodes = pyzbar.decode(frame)
         for barcode in barcodes:
             # decode data input
             decodedCode = unquote(barcode.data.decode())
             print("decoded:")
             print(decodedCode)
+            # split input data on '}' (according to protocol)
             dataArray = decodedCode.split('}')
             try:
                 checkString = dataArray[0]
+                # check if checksum is equal to 'BAU' (according to protocol)
                 if checkString != "BAU":
                     print('Wrong code')
                     break
+                # create a new person with given data
                 new_person = Person(dataArray[1], dataArray[2], dataArray[3], dataArray[4],
                                     dataArray[5], dataArray[6], dataArray[7], dataArray[8])
-                print("new_person")
-                print(dataArray[8])
                 # check if new_person is already contained in persons list
                 if any(person for person in persons if (person.__repr__() == new_person.__repr__())):
                     print('Person already registered')
                     # wait 2 seconds
                     time.sleep(2.0)
                     break
+                # add new person to persons list
                 persons.append(new_person)
+                # activate led to indicate a new record
                 led.on()
                 gui.write(persons)
                 gui.personGescannt(new_person)
                 # serialize persons list (open file in write-binary-mode)
                 pickle.dump(persons, open(dateFile, 'wb'))
-                print(persons)
-                #time.sleep(2.0)
+                # deactive led
                 led.off()
-                print('Scan:')
             except IndexError:
+                # no valid QR code found
                 print('Malicious QR-Code')
 
 
 if __name__ == "__main__":
-    #if os.environ.get('DISPLAY', '') == '':
-    #    print('no display found. Using :0.0')
-    #    os.environ.__setitem__('DISPLAY', ':0.0')
-
-
     start = Tk()
-    startGui = startGrafik.startGui(start)
+    startGui = indexGraphic.startGui(start)
+    # show label for choosing mode for the scanner
     startGui.auswahlTreffen()
     start.update()
 
-    # wait for input of mode (A: scan, D: print)
+    # wait for input of mode (A: scan / D: print)
     kp = Keypad.Keypad(keys, rowsPins, colsPins, ROWS, COLS)
     kp.setDebounceTime(50)
     mode = None
     while mode != 'A' and mode != 'D':
         print('Wähle Modus (A: Scannen, D: Drucken): ')
         mode = inputDigit(kp)
-        print(mode)
     print('Mode: ' + mode)
 
     # wait for input of date
     matchObject = None
     while not matchObject:
         date = dateInput(kp, startGui, start)
-        print(date)
         matchObject = re.match(
             "^[0-9][0-9]\*[0-9][0-9]\*[0-9][0-9][0-9][0-9]$", date)
         if not matchObject:
             print('Falsches Datum!')
+    # generate file name for input date
     dateReplaced = date.replace('*', '_')
     dateFile = dateReplaced + '.p'
 
     if mode == 'A':
+        # activated scan mode
         try:
             # close input window
             start.destroy()
             # create gui
             global gui
             main = Tk()
-            mainGUI = grafikinterface_new.mainGui(main, 2)
-            # start scanner thread
+            mainGUI = scanGraphic.mainGui(main, 2)
+            # start scanner thread until a KeyboardInterrupt appears
             try:
                 scanThread = threading.Thread(
                     target=scan, args=(mainGUI, dateFile, 17), daemon=True)
@@ -173,19 +169,21 @@ if __name__ == "__main__":
         finally:
             GPIO.cleanup([7, 8, 9, 10, 11, 15, 18, 25])
     elif mode == 'D':
+        # activated print mode
+        # load serialized persons list
         persons = pickle.load(open(dateFile, 'rb'))
         table = []
         for person in persons:
             table.append(person.getList())
         headers = ["Vorname", "Nachname", "GebDatum",
                    "TelefonNr", "Straße", "HausNr", "PLZ", "Ort"]
+        # create html file from serialized data
         tableHTML = tabulate(table, headers, tablefmt="html")
         # head
         head = "<head><meta charset='utf-8'/></head>"
         tableHTML = head + tableHTML
-        # css
+        # css rules for html table
         css = "<style>table {border-spacing: 15px 10px;border-collapse: separate;}th {font-size: 20px;line-height: 1.5;}tr {background-color: antiquewhite;}</style>"
         tableHTML = tableHTML + css
-        #print(tableHTML)
-        # write to pdf
+        # write table to pdf
         pdfkit.from_string(tableHTML, dateReplaced + '.pdf')
